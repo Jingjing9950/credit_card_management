12c0681fe139e6710e04a41f04fdbb0122098dc9
Jingjing <fanghongwei@Macs-MacBook-Pro.local>
2018-07-10 22:04:13 +0800

    Init commit

---
+++ src/main/java/com/lafoo/icard/IcardApplication.java
@@ -0,0 +1,12 @@
+package com.lafoo.icard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcardApplication.class, args);
+    }
+}
